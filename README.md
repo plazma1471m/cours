4a0559f880354026a1bce3dc7f99bd2e8a253658
plazma1471m <47203371+plazma1471m@users.noreply.github.com>
2019-01-31 09:41:12 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+<a href="http://en.wikipedia.org/wiki/Cross-origin_resource_sharing">CORS</a> is a technology specification to get around the *Same Origin Policy* restrictions of web browsers. To allow cross domain sharing, web servers have to attach additional headers. Unfortunately, not many servers have done this yet, and hence we have issues trying to access their resources in certain scenarios such as WebGL. This simple nodejs proxy will attach the CORS headers so that the browser thinks it is okay to be shared. 
+
+For example:
+
+http://corsproxy.nodester.com/?src=farm7.static.flickr.com/6048/6250681839_7574c8c4b6_m.jpg
+
+<img src="http://corsproxy.nodester.com/?src=farm7.static.flickr.com/6048/6250681839_7574c8c4b6_m.jpg"></img>
+
+Note: The above link is now defunct, looks like nodester went out of business. However, it still illustrates the point how you can use this service. 
---
+++ server.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var url = require('url');
+
+var port = process.env.PORT || 12345;
+http.createServer(function(proxyReq, proxyResp) {
+    var params = url.parse(proxyReq.url, true);
+    var imgURL = params.query.src;
+
+    var destParams = url.parse(imgURL);
+
+    var reqOptions = {
+        host : destParams.host,
+        port : 80,
+        path : destParams.path,
+        method : "GET"
+    };
+
+    var req = http.request(reqOptions, function(res) {
+        var headers = res.headers;
+        headers['Access-Control-Allow-Origin'] = '*';
+        headers['Access-Control-Allow-Headers'] = 'X-Requested-With';
+        proxyResp.writeHead(200, headers);
+
+        res.on('data', function(chunk) {
+            proxyResp.write(chunk);
+        });
+
+        res.on('end', function() {
+            proxyResp.end();
+        });
+    });
+
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+        proxyResp.writeHead(503);
+        proxyResp.write("An error happened!");
+        proxyResp.end();
+    });
+    req.end();
+
+}).listen(port);
